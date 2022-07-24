896e4245c430442ee40c3eb7ccb773eae1d68414
Benjamin Neff <benjamin@coding4coffee.ch>
2022-07-25 01:27:35 +0200

    Remove Style/SingleLineMethods rule to prepare for prettier

--- test/dummy/app/models/person.rb
+++ test/dummy/app/models/person.rb
@@ -7,24 +7,52 @@ class Person
     @guid = UUID.generate(:compact)
   end
 
-  def private_key; OpenSSL::PKey::RSA.new(serialized_private_key) end
-  def public_key;  OpenSSL::PKey::RSA.new(serialized_public_key) end
+  def private_key
+    OpenSSL::PKey::RSA.new(serialized_private_key)
+  end
+  def public_key
+    OpenSSL::PKey::RSA.new(serialized_public_key)
+  end
 
-  def alias_url;     "#{url}people/#{guid}" end
-  def hcard_url;     "#{url}hcard/users/#{guid}" end
-  def profile_url;   "#{url}u/#{nickname}" end
-  def atom_url;      "#{url}public/#{nickname}.atom" end
-  def salmon_url;    "#{url}receive/users/#{guid}" end
-  def subscribe_url; "#{url}people?q={uri}" end
+  def alias_url
+    "#{url}people/#{guid}"
+  end
+  def hcard_url
+    "#{url}hcard/users/#{guid}"
+  end
+  def profile_url
+    "#{url}u/#{nickname}"
+  end
+  def atom_url
+    "#{url}public/#{nickname}.atom"
+  end
+  def salmon_url
+    "#{url}receive/users/#{guid}"
+  end
+  def subscribe_url
+    "#{url}people?q={uri}"
+  end
 
-  def nickname; diaspora_id.split("@")[0] end
+  def nickname
+    diaspora_id.split("@")[0]
+  end
 
-  def photo_default_url; "#{url}assets/user/default.png" end
+  def photo_default_url
+    "#{url}assets/user/default.png"
+  end
 
-  def searchable; true end
-  def full_name;  "Dummy User" end
-  def first_name; "Dummy" end
-  def last_name;  "User" end
+  def searchable
+    true
+  end
+  def full_name
+    "Dummy User"
+  end
+  def first_name
+    "Dummy"
+  end
+  def last_name
+    "User"
+  end
 
   def save!
     Person.database[:diaspora_id][diaspora_id] = self
